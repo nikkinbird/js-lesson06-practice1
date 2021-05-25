19f139c76474dda3a85e3ccdfd9c95b124df10fa
nikkinbird <nikki.bollman@gmail.com>
2021-05-24 20:25:19 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var shoutIt = function (name) {
+  var shout = `Hello ${name}, nice to meet you!`.toUpperCase();
+  return shout;
+};
+
+console.log(shoutIt("Nikki"));
+console.log(shoutIt("Millie"));
+console.log(shoutIt("Snowball"));
+console.log(shoutIt("Smolcat"));
